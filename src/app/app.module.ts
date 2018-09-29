88070f7220e5dcd0e0af7b1cc2459171600c4556
MichelM279 <mi.mangold89@gmail.com>
2018-09-29 14:36:23 +0200

    Routing and Mockups

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,66 @@
+import {MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { DoctorsComponent } from './doctors/doctors.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routerConfig: Routes = [
+  {
+      path: 'home',
+      component: HomeComponent
+  },
+  {
+      path: 'about',
+      component: AboutComponent
+  },
+  {
+      path: 'doctors',
+      component: DoctorsComponent
+  },
+  {
+      path: 'services',
+      component: ServicesComponent
+  },
+  {
+      path: 'contact',
+      component: ContactComponent
+  },
+  {
+      path: '',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  },
+  {
+      path: '**',
+      redirectTo: '/home',
+      pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    DoctorsComponent,
+    ServicesComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    RouterModule.forRoot(routerConfig),
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
