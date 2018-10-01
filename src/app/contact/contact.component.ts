aee7afe7579ac812c43f51088f1ea6675c61abcc
MichelM279 <mi.mangold89@gmail.com>
2018-10-01 12:16:04 +0200

    fixed embeddings and improved contact

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
@@ -10,7 +11,19 @@ export class ContactComponent implements OnInit {
   ngOnInit() {
   }
 
-  constructor() {
+  constructor(private router: Router) {}
+
+  onCallClick() {
+    location.href = 'tel:+1 876-972-1933';
+  }
+
+  onMailClick() {
+    location.href = 'mailto:xyz@abc.com';
+    // window.open('mailto:info@phoenixmedicalcenter.com');
+  }
+
+  onPositionClick() {
+    this.router.navigate(['../contact']);
   }
 
 }
